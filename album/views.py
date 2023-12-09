c2f41fcb6b42f89b774cd28f73ee7a89f6ca708b
Abdul Khaleq <abdulkhaleq6008@gmail.com>
2023-12-09 23:57:48 +0600

    added delete button

--- album/views.py
+++ album/views.py
@@ -4,6 +4,7 @@ from . import models
 
 # Create your views here.
 def add_album(request):
+    title = 'Add Abum'
     if request.method == 'POST':
         album_form = forms.AlbumForm(request.POST)
         if album_form.is_valid():
@@ -11,9 +12,10 @@ def add_album(request):
             return redirect('homepage')
     else:
         album_form = forms.AlbumForm()
-    return render(request, 'add_album.html', {'form': album_form})
+    return render(request, 'add_album.html', {'form': album_form, 'title':title})
 
 def edit_album(request, id):
+    title = 'Edit Abum'
     album = models.Album.objects.get(pk=id)
     album_form = forms.AlbumForm(instance=album)
     if request.method == 'POST':
@@ -21,4 +23,4 @@ def edit_album(request, id):
         if album_form.is_valid():
             album_form.save()
             return redirect('homepage')
-    return render(request, 'add_album.html', {'form': album_form})
+    return render(request, 'add_album.html', {'form': album_form, 'title':title})
